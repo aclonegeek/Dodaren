178f1aa82148bcc73cb5ee89cbbdf0f424942f63
Randy Taylor <tehgecKozzz@gmail.com>
2016-08-31 22:00:23 -0400

    Implement Entity class

---
+++ Dodaren/Entities/Entity.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#include "../ResourceHandler.hpp"
+
+class Entity {
+public:
+	Entity() {}
+
+	virtual void load(Textures::ID id, const std::string& filePath) {
+		textureMap.load(id, filePath);
+		m_sprite.setTexture(m_texture);
+	}
+
+	virtual void update(const sf::Time& dt) {
+		m_sprite.move(m_velocity * dt.asSeconds());
+	}
+
+	virtual void destroy() {
+		m_active = 0;
+	}
+
+	virtual void collision(Entity& entity) = 0;
+
+	bool checkCollision(Entity& entity) {
+		return m_sprite.getGlobalBounds().intersects(entity.getSprite().getGlobalBounds());
+	}
+
+	void setVelocity(sf::Vector2f velocity) { m_velocity = velocity; }
+	void setVelocity(float vx, float vy)	{ m_velocity.x = vx; m_velocity.y = vy; }
+
+	const bool			isActive()	  const	{ return m_active == 1; }
+	const std::string	getType()	  const	{ return m_type; }
+	const sf::Sprite	getSprite()	  const	{ return m_sprite; }
+	const sf::Vector2f	getVelocity() const	{ return m_velocity; }
+protected:
+	int				m_active;
+	std::string		m_type;
+
+	sf::Sprite		m_sprite;
+	sf::Vector2f	m_velocity;
+private:
+	sf::Texture		m_texture;
+};
