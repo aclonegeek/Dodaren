e82de330ad9126f14a0fdfe9cfdf81f1ac2ab1c5
Randy Taylor <tehgecKozzz@gmail.com>
2016-09-03 14:40:51 -0400

    Various changes Implement input handling for Player Implement idle animations Fix entire spritesheet displaying until keypress

--- Dodaren/Entities/Player.cpp
+++ Dodaren/Entities/Player.cpp
@@ -1,22 +1,46 @@
 #include "Player.hpp"
 
+#include <iostream>
+
 Player::Player(EntityManager& entityManager, sf::Vector2f& windowSize)
 	: m_entityManager{ entityManager }
 	, m_windowSize{ windowSize } {
 	m_active = 1;
 	m_type = "Player";
 	load(Textures::ID::Player, "Resources/Graphics/Sprites/spritesheet.png");
+	m_sprite.setTextureRect({ 32, 64, 32, 32 });
 
-	m_animation = std::make_unique<Animation>(
-		m_sprite, 0.10f, 0, 0, 32, 32, 300, 300);
+	m_animation = std::make_unique<Animation>(m_sprite, 0.10f, 0, 0, 32, 32, 300, 300);
 	setupAnimations();
 }
 
 void Player::setupAnimations() {
+	m_animation->add("IdleLeft", 1, 32, 32, 32, 32);
+	m_animation->add("IdleRight", 1, 32, 64, 32, 32);
 	m_animation->add("WalkLeft", 3, 0, 32, 32, 32);
 	m_animation->add("WalkRight", 3, 0, 64, 32, 32);
+
+	m_animation->play("IdleRight");
+	m_facing = Direction::RIGHT;
+}
+
+void Player::handleInput(const sf::Time& dt) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		m_animation->play("WalkLeft");
+		m_facing = Direction::LEFT;
+	} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		m_animation->play("WalkRight");
+		m_facing = Direction::RIGHT;
+	} else {
+		if (m_facing == Direction::LEFT) {
+			m_animation->play("IdleLeft");
+		} else {
+			m_animation->play("IdleRight");
+		}
+	}
 }
 
 void Player::update(const sf::Time& dt) {
-	m_animation->play("WalkRight", dt);
+	handleInput(dt);
+	m_animation->update(dt);
 }
--- Dodaren/Entities/Player.hpp
+++ Dodaren/Entities/Player.hpp
@@ -2,15 +2,29 @@
 #include "../Animation.hpp"
 #include "EntityManager.hpp"
 
+// todo: fix idle animations
+
+enum class Direction {
+	LEFT,
+	RIGHT
+};
+
 class Player : public Entity {
 public:
 	Player(EntityManager& entityManager, sf::Vector2f& windowSize);
 
 	void setupAnimations();
+	void handleInput(const sf::Time& dt);
 	void update(const sf::Time& dt);
 private:
+	void moveLeft();
+	void moveRight();
+	void stopMoving();
+
 	EntityManager& m_entityManager;
 	sf::Vector2f& m_windowSize;
 
 	std::unique_ptr<Animation> m_animation;
+
+	Direction m_facing;
 };
